1cea5a9f7a951e06c5e4c2a82814c28fb6a087e1
unknown <shammo12345@hotmail.com>
2020-12-07 15:19:07 -0500

    Makes Radiology and AI pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Radiology-and-AI',
+    version='1.0',
+    packages=['Radiology-and-AI'],
+    url='https://github.com/McMasterAI/Radiology-and-AI.git',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
