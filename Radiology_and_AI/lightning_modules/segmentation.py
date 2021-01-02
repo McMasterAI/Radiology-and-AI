8a663535eb7cb7195c9809b5a74bdc0d0ac36071
Apeirogon <som5@mcmaster.ca>
2021-01-01 21:56:22 -0500

    Update brats training notebook for webdataset object

--- Radiology_and_AI/lightning_modules/segmentation.py
+++ Radiology_and_AI/lightning_modules/segmentation.py
@@ -6,25 +6,76 @@ class TumourSegmentation(pl.LightningModule):
   def __init__(self,model):
     super().__init__()
     self.model = model
+  
   def forward(self,x):
   #  x=x.half()
+
     f = self.model.forward(x)
 
   #  print('Done forward step!')
     return f
 
   def training_step(self, batch, batch_idx):
-
     x, y = batch
+    x = torch.unsqueeze(x, axis=0)
+    y = torch.unsqueeze(y, axis=0)
+    #print(x.shape)
 
-    y_hat = self.model(x)
-    
-    # I'm not really sure why the shape is weird here, but this seems to run
-    y_hat = torch.squeeze(y_hat,axis=1) 
+    y_hat = self.forward(x)
 
-    loss = torch.mean(torch.abs(y_hat - y))
-    # this CE results in a CUDA error because the U-net implementation is strange
-    #F.binary_cross_entropy(y_hat, y)
+    #plt.imshow(y_hat[0, 0, 120], cmap='')
+    #plt.imshow(y_hat.cpu()[0, 1, 120])
+    #plt.imshow(y_hat[0, 2, 120])
+    #plt.imshow(y_hat[0, 3, 120])
+    #plt.show()
+
+    shape = list(y.size())
+    shape[1] = 3
+    zeros = torch.zeros(shape).cuda()
+
+    for i in range(1, 4):
+      zeros[:, i-1][torch.squeeze(y == i, dim=1)] = 1
+
+  # basic mean of all channels for now
+    loss = compute_per_channel_dice(y_hat, zeros)
+    loss[loss != loss] = 0
+    print('Training loss: ')
+    print(loss)
+    loss = torch.sum(loss)
+    self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
     return loss
+
+  def validation_step(self, batch, batch_idx):
+    x, y = batch
+    x = torch.unsqueeze(x, axis=0)
+    y = torch.unsqueeze(y, axis=0)
+    #print(x.shape)
+
+    y_hat = self.forward(x)
+
+    #plt.imshow(y_hat[0, 0, 120], cmap='')
+    #plt.imshow(y_hat.cpu()[0, 1, 120])
+    #plt.imshow(y_hat[0, 2, 120])
+    #plt.imshow(y_hat[0, 3, 120])
+    #plt.show()
+
+    shape = list(y.size())
+    shape[1] = 3
+    zeros = torch.zeros(shape).cuda()
+
+    for i in range(1, 4):
+      zeros[:, i-1][torch.squeeze(y == i, dim=1)] = 1
+
+  # basic mean of all channels for now
+    loss = compute_per_channel_dice(y_hat, zeros)
+    loss[loss != loss] = 0
+    print('Validation loss: ')
+    print(loss)
+    loss = torch.sum(loss)
+    self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
+    return loss
+
   def configure_optimizers(self):
       return torch.optim.Adam(self.parameters(), lr=0.02)
