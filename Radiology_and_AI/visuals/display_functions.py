1bc5d31275adc7d49b53ceabf29a38ceff55ba36
dufaultc <dufault77@gmail.com>
2021-01-28 15:07:18 -0500

    the last commit was a lie

--- Radiology_and_AI/visuals/display_functions.py
+++ Radiology_and_AI/visuals/display_functions.py
@@ -17,15 +17,15 @@ def display_brain_and_segs(seg_1,seg_2 = numpy.array([None]),mri_chan=numpy.arra
 
 def gen_gif(out_dir,seg_1,seg_2 = numpy.array([None]),mri_chan=numpy.array([None]),downsize_factor=5,angle_num = 20,fig_size=(50,25)): 
     for angle in range(0, 360, angle_num):
-        ax = make_ax(True,fig_size=(50,25))
+        ax = make_ax(True,fig_size=fig_size)
         seg_1_small = make_smaller(seg_1,downsize_factor)
         ax.voxels(seg_1_small, edgecolors='gray', shade=False)
         if (seg_2.any() == None) == False:
             seg_2_small = make_smaller(seg_2,downsize_factor)
             ax.voxels(seg_2_small, facecolors='#1f77b430', shade=False)
         if (mri_chan.any() == None) == False:
-            mri_chan_small = make_smaller(seg_2,downsize_factor) 
-            ax.voxels(mri_chan_small, facecolors='#3eb19c20', shade=False)   
+            mri_chan_small = make_smaller(mri_chan,downsize_factor) 
+            ax.voxels(mri_chan_small, facecolors='#3eb19c20', shade=False) 
         ax.view_init(30, angle)
         fig1 = plt.gcf()
         fig1.savefig(f'{outdir}/{angle}.png')
